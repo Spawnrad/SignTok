886292621d3ae463060387acc4f34c234ddd0e6b
Spawnrad <spawnrad@gmail.com>
2023-03-13 17:10:04 +0100

    Update useragent

--- src/Utils.js
+++ src/Utils.js
@@ -1,9 +1,4 @@
 class Utils {
-  static verify_fp() {
-    // TODO, add proper verify fp method
-    return "verify_l9crwi93_kIOkZI3S_l9I2_4ARw_8GFp_IvNf0zaXa6DI";
-  }
-
   static makePayload(data, navigator) {
     return JSON.stringify({
       status: "ok",
