734964fad99b02accc74ad294a59b56263537b8f
Spawnrad <spawnrad@gmail.com>
2023-04-17 20:44:50 +0200

    Update UserAgent

--- src/Signer.js
+++ src/Signer.js
@@ -4,7 +4,7 @@ const { createCipheriv } = require("crypto");
 
 class Signer {
   static DEFAULT_USERAGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
   static PASSWORD = "webapp1.0+202106";
   /**
    * @type Window
